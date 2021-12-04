f22d4c59a2e7a899db81bbfae47c8241f12be247
Qianhui Wang <u7070170@anu.edu.au>
2021-12-04 21:18:50 +0800

    bayesian network

--- src3/bayesnet/likelihood.py
+++ src3/bayesnet/likelihood.py
@@ -1,6 +1,10 @@
 from model import model
 
 # Calculate probability for a given observation
-probability = model.probability([["none", "no", "on time", "attend"]])
+probability = model.probability([
+  ["none", "no", "on time", "attend"],
+  ["none", "no", "on time", "miss"],
+  ["none", "yes", "delayed", "attend"]
+])
 
 print(probability)
